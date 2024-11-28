d20b9853a28f5725eca13dc0bdb3133cb8b295a8
chuanenlin <david.lin0424@gmail.com>
2024-11-28 22:26:22 +0800

    Initial commit

--- Frontend/node_modules/uniforms-mui/esm/ValidatedForm.js
+++
@@ -1,9 +0,0 @@
-import { ValidatedForm } from 'uniforms';
-import BaseForm from './BaseForm';
-function Validated(parent) {
-    class _ extends ValidatedForm.Validated(parent) {
-    }
-    _.Validated = Validated;
-    return _;
-}
-export default Validated(BaseForm);
